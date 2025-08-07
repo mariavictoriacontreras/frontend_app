c9cfdb74de42a5f48772b8c0e7b0a622d11d01cf
Magali Vivas <magali@psiware.com.ar>
2025-08-07 11:55:28 -0300

    add config initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// src/App.tsx
+import { Routes, Route } from 'react-router-dom';
+// import UserList from './pages/UserList.tsx';
+// import UserForm from './pages/UserForm.tsx';
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/users" element={<UserList />} />
+      <Route path="/users/new" element={<UserForm />} />
+      <Route path="/users/:id" element={<UserForm />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
