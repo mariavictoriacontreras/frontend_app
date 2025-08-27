ac8a8134af3893bf38778edacaaceda59b168878
Magali Vivas <magali@psiware.com.ar>
2025-08-27 14:58:03 -0300

    edit app for comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserList from "./pages/UserList";
-import UserForm from "./pages/UserForm";
+// import UserList from "./pages/UserList";
+// import UserForm from "./pages/UserForm";
 import './styles/theme.scss';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/users" element={<UserList />} />
+        {/* <Route path="/users" element={<UserList />} />
         <Route path="/users/new" element={<UserForm />} />
-        <Route path="/users/edit/:id" element={<UserForm />} />
+        <Route path="/users/edit/:id" element={<UserForm />} /> */}
       </Routes>
     </Router>
   );
